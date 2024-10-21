db86d41b44b9be7e73a957be639dac2a78e42412
milk953 <milk.the@hotmail.com>
2024-10-21 16:27:11 +0700

    addnewpage

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Layout } from 'antd';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './Header/Header';
 import RDESMasterUpload from './RDESMasterUpload/RDESMasterUpload';
+import Shipmentschedulemaintain from './Shipment schedule maintain/Shipmentschedulemaintain';
 import axios from "axios";
 
 const { Content } = Layout;
@@ -26,7 +27,7 @@ const App = () => {
           <Content style={{ padding: '90px 15px 30px 15px', minHeight: '100vh',height:'',fontFamily: 'Varela Round, sans-serif'}}>
             <Routes>
               <Route path="/CommonSystem/ConditionSystem/RDESMasterUpload" element={<RDESMasterUpload />} />
-
+              <Route path="/CommonSystem/ConditionSystem/Shipment schedule maintain" element={<Shipmentschedulemaintain />} />
             </Routes>
           </Content>
         </Layout>
