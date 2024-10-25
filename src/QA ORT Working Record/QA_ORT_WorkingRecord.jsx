53f77dd2e227ec6e5ea24acad31bd3d4eb1e4086
khunpan9551 <paiboon.wongthongdee@gmail.com>
2024-10-25 13:18:41 +0700

    commit by khun

--- src/QA ORT Working Record/QA_ORT_WorkingRecord.jsx
+++ src/QA ORT Working Record/QA_ORT_WorkingRecord.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from "react";
+import {
+  Layout,
+  theme,
+  Input,
+  Button,
+  // Table,
+  Avatar,
+  Card,
+  Select,
+  Row,
+  Col,
+  DatePicker,
+} from "antd";
+const { Content } = Layout;
 
 function QA_ORT_WorkingRecord() {
   return (
-    <div>QA_ORT_WorkingRecord</div>
-  )
+    <div>
+      <Content>
+        <h1>QA ORT WORKING RECORD</h1>
+        <Row>
+            <Col span={2}></Col>
+        </Row>
+        {/* <div style={{ marginLeft: "30px", marginBottom: "5px" }}>
+          <span style={{ fontSize: "18px" }}>Factory</span>{" "}
+        </div> */}
+      </Content>
+    </div>
+  );
 }
 
-export default QA_ORT_WorkingRecord
+export default QA_ORT_WorkingRecord;
