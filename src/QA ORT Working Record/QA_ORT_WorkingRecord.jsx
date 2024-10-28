95531f796f2d9effb4035d770182567885ef23d4
khunpan9551 <paiboon.wongthongdee@gmail.com>
2024-10-28 13:25:03 +0700

    commit by khun

--- src/QA ORT Working Record/QA_ORT_WorkingRecord.jsx
+++ src/QA ORT Working Record/QA_ORT_WorkingRecord.jsx
@@ -27,9 +27,43 @@ import {
 } from "@ant-design/icons";
 import "./QA_ORT_WorkingRecord.css";
 import { fn_QA_ORT_WorkingRecord } from "./fn_QA_ORT_WorkingRecord";
+import moment from "moment";
 
 function QA_ORT_WorkingRecord() {
-  const {options} = fn_QA_ORT_WorkingRecord();
+  const {
+    opFactory,
+    opProduct_Type,
+    opInput,
+    opOutput,
+    setSelectedFromIn,
+    selectedDateFromIn,
+    setSelectedFromOut,
+    selectedDateFromOut,
+    setSelectedToIn,
+    selectedDateToIn,
+    setSelectedToOut,
+    selectedDateToOut,
+    drpFactory,
+    setDrpFactory,
+    drpProductType,
+    setDrpProductType,
+    drpInPut,
+    setDrpInPut,
+    drpOutPut,
+    setDrpOutPut,
+    setInputProductName,
+    inputProductName,
+    inputTestItem,
+    setInputTestItem,
+    inputLotNo,
+    setInputLotNo,
+    inputWeekNo,
+    setInputWeekNo,
+    inputSerialNo,
+    setInputSerialNo,
+    Btn_Search,
+    Btn_Cancel,
+  } = fn_QA_ORT_WorkingRecord();
   return (
     <div>
       {/* <Content>
@@ -47,16 +81,29 @@ function QA_ORT_WorkingRecord() {
           <Select
             size="middle"
             showSearch
-            allowClear
             style={{
               width: "96%",
             }}
             placeholder="--- select ---"
             optionFilterProp="label"
             filterSort={(optionA, optionB) =>
-              (optionA?.label ?? "").toLowerCase().localeCompare((optionB?.label ?? "").toLowerCase())
+              (optionA?.label ?? "")
+                .toLowerCase()
+                .localeCompare((optionB?.label ?? "").toLowerCase())
             }
-            options={options} 
+            options={opFactory.map((factory) => ({
+              value: factory.value,
+              label: factory.label,
+            }))}
+            value={drpFactory === "" ? undefined : drpFactory}
+            onChange={(e) => {
+              if (e) {
+                setDrpFactory(e);
+              } else {
+                setDrpFactory("");
+              }
+            }}
+            allowClear
           />
         </Col>
         <Col span={4}>
@@ -64,7 +111,6 @@ function QA_ORT_WorkingRecord() {
           <Select
             size="middle"
             showSearch
-            allowClear
             style={{
               width: "96%",
             }}
@@ -75,6 +121,19 @@ function QA_ORT_WorkingRecord() {
                 .toLowerCase()
                 .localeCompare((optionB?.label ?? "").toLowerCase())
             }
+            options={opProduct_Type.map((product_type) => ({
+              value: product_type.value,
+              label: product_type.label,
+            }))}
+            value={drpProductType === "" ? undefined : drpProductType}
+            onChange={(e) => {
+              if (e) {
+                setDrpProductType(e);
+              } else {
+                setDrpProductType("");
+              }
+            }}
+            allowClear
           />
         </Col>
         <Col span={4}>
@@ -82,7 +141,6 @@ function QA_ORT_WorkingRecord() {
           <Select
             size="middle"
             showSearch
-            allowClear
             style={{
               width: "96%",
             }}
@@ -93,6 +151,19 @@ function QA_ORT_WorkingRecord() {
                 .toLowerCase()
                 .localeCompare((optionB?.label ?? "").toLowerCase())
             }
+            options={opInput.map((input) => ({
+              value: input.value,
+              label: input.label,
+            }))}
+            value={drpInPut === "" ? undefined : drpInPut}
+            onChange={(e) => {
+              if (e) {
+                setDrpInPut(e);
+              } else {
+                setDrpInPut("");
+              }
+            }}
+            allowClear
           />
         </Col>
         <Col span={4}>
@@ -103,6 +174,19 @@ function QA_ORT_WorkingRecord() {
             style={{
               width: "96%",
             }}
+            value={
+              selectedDateFromIn === null
+                ? undefined
+                : moment(selectedDateFromIn, "YYYY-MM-DD")
+            }
+            onChange={(date) => {
+              if (date) {
+                setSelectedFromIn(date.format("YYYYMMDD"));
+              } else {
+                setSelectedFromIn(null);
+              }
+            }}
+            allowClear
           />
         </Col>
         <Col span={4}>
@@ -113,6 +197,19 @@ function QA_ORT_WorkingRecord() {
             style={{
               width: "96%",
             }}
+            value={
+              selectedDateToIn === null
+                ? undefined
+                : moment(selectedDateToIn, "YYYY-MM-DD")
+            }
+            onChange={(date) => {
+              if (date) {
+                setSelectedToIn(date.format("YYYYMMDD"));
+              } else {
+                setSelectedToIn(null);
+              }
+            }}
+            allowClear
           />
         </Col>
         <Col span={4} className="Center">
@@ -121,6 +218,7 @@ function QA_ORT_WorkingRecord() {
             type="primary"
             className="Btn_Search"
             icon={<SearchOutlined className="Icon_Size" />}
+            onClick={Btn_Search}
           >
             Search
           </Button>
@@ -132,7 +230,6 @@ function QA_ORT_WorkingRecord() {
           <Select
             size="middle"
             showSearch
-            allowClear
             style={{
               width: "96%",
             }}
@@ -143,6 +240,19 @@ function QA_ORT_WorkingRecord() {
                 .toLowerCase()
                 .localeCompare((optionB?.label ?? "").toLowerCase())
             }
+            options={opOutput.map((output) => ({
+              value: output.value,
+              label: output.label,
+            }))}
+            value={drpOutPut === "" ? undefined : drpOutPut}
+            onChange={(e) => {
+              if (e) {
+                setDrpOutPut(e);
+              } else {
+                setDrpOutPut("");
+              }
+            }}
+            allowClear
           />
         </Col>
         <Col span={4}>
@@ -153,6 +263,19 @@ function QA_ORT_WorkingRecord() {
             style={{
               width: "96%",
             }}
+            value={
+              selectedDateFromOut === null
+                ? undefined
+                : moment(selectedDateFromOut, "YYYY-MM-DD")
+            }
+            onChange={(date) => {
+              if (date) {
+                setSelectedFromOut(date.format("YYYYMMDD"));
+              } else {
+                setSelectedFromOut(null);
+              }
+            }}
+            allowClear
           />
         </Col>
         <Col span={4}>
@@ -163,6 +286,19 @@ function QA_ORT_WorkingRecord() {
             style={{
               width: "96%",
             }}
+            value={
+              selectedDateToOut === null
+                ? undefined
+                : moment(selectedDateToOut, "YYYY-MM-DD")
+            }
+            onChange={(date) => {
+              if (date) {
+                setSelectedToOut(date.format("YYYYMMDD"));
+              } else {
+                setSelectedToOut(null);
+              }
+            }}
+            allowClear
           />
         </Col>
         <Col span={4}>
@@ -172,6 +308,15 @@ function QA_ORT_WorkingRecord() {
             style={{
               width: "96%",
             }}
+            value={inputProductName === "" ? undefined : inputProductName}
+            onChange={(e) => {
+              if (e.target.value) {
+                setInputProductName(e.target.value);
+              } else {
+                setInputProductName(null);
+              }
+            }}
+            allowClear
           />
         </Col>
         <Col span={4}>
@@ -181,6 +326,15 @@ function QA_ORT_WorkingRecord() {
             style={{
               width: "96%",
             }}
+            value={inputTestItem === "" ? undefined : inputTestItem}
+            onChange={(e) => {
+              if (e.target.value) {
+                setInputTestItem(e.target.value);
+              } else {
+                setInputTestItem(null);
+              }
+            }}
+            allowClear
           />
         </Col>
         <Col span={4} className="Center">
@@ -189,6 +343,7 @@ function QA_ORT_WorkingRecord() {
             type="primary"
             className="Btn_Cancel"
             icon={<RedoOutlined className="Icon_Size" />}
+            onClick={Btn_Cancel}
           >
             Cancel
           </Button>
@@ -202,6 +357,15 @@ function QA_ORT_WorkingRecord() {
             style={{
               width: "96%",
             }}
+            value={inputLotNo === "" ? undefined : inputLotNo}
+            onChange={(e) => {
+              if (e.target.value) {
+                setInputLotNo(e.target.value);
+              } else {
+                setInputLotNo(null);
+              }
+            }}
+            allowClear
           />
         </Col>
         <Col span={4}>
@@ -211,6 +375,15 @@ function QA_ORT_WorkingRecord() {
             style={{
               width: "96%",
             }}
+            value={inputWeekNo === "" ? undefined : inputWeekNo}
+            onChange={(e) => {
+              if (e.target.value) {
+                setInputWeekNo(e.target.value);
+              } else {
+                setInputWeekNo(null);
+              }
+            }}
+            allowClear
           />
         </Col>
         <Col span={4}>
@@ -220,6 +393,15 @@ function QA_ORT_WorkingRecord() {
             style={{
               width: "96%",
             }}
+            value={inputSerialNo === "" ? undefined : inputSerialNo}
+            onChange={(e) => {
+              if (e.target.value) {
+                setInputSerialNo(e.target.value);
+              } else {
+                setInputSerialNo(null);
+              }
+            }}
+            allowClear
           />
         </Col>
         <Col span={8}></Col>
--- src/QA ORT Working Record/fn_QA_ORT_WorkingRecord.jsx
+++ src/QA ORT Working Record/fn_QA_ORT_WorkingRecord.jsx
@@ -1,11 +1,129 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 function fn_QA_ORT_WorkingRecord() {
-  const [options, setOptions] = useState([
+  const [opFactory, setOpFactory] = useState([
     { value: "A1", label: "A1" },
     { value: "N1", label: "N1" },
   ]);
-  return { options };
+  const [opProduct_Type, setOpProduct_Type] = useState([
+    { value: "ALL", label: "ALL" },
+    { value: "Bare", label: "Bare" },
+    { value: "Assy", label: "Assy" },
+  ]);
+  const [opInput, setOpInput] = useState([
+    { value: "INPUT1", label: "INPUT1" },
+    { value: "INPUT2", label: "INPUT2" },
+  ]);
+  const [opOutput, setOpOutput] = useState([
+    { value: "OUTPUT1", label: "OUTPUT1" },
+    { value: "OUTPUT2", label: "OUTPUT2" },
+  ]);
+  const [selectedDateFromIn, setSelectedFromIn] = useState(null);
+  const [selectedDateFromOut, setSelectedFromOut] = useState(null);
+  const [selectedDateToIn, setSelectedToIn] = useState(null);
+  const [selectedDateToOut, setSelectedToOut] = useState(null);
+  const [drpFactory, setDrpFactory] = useState("");
+  const [drpProductType, setDrpProductType] = useState("");
+  const [drpInPut, setDrpInPut] = useState("");
+  const [drpOutPut, setDrpOutPut] = useState("");
+  const [inputProductName, setInputProductName] = useState(null);
+  const [inputTestItem, setInputTestItem] = useState(null);
+  const [inputLotNo, setInputLotNo] = useState(null);
+  const [inputWeekNo, setInputWeekNo] = useState(null);
+  const [inputSerialNo, setInputSerialNo] = useState(null);
+
+  const Btn_Search = async () => {
+    console.log(
+      "Btn_Search",
+      selectedDateFromIn,
+      selectedDateFromOut,
+      selectedDateToIn,
+      selectedDateToOut,
+      drpFactory,
+      drpProductType,
+      drpInPut,
+      drpOutPut,
+      inputProductName,
+      inputTestItem,
+      inputLotNo,
+      inputWeekNo,
+      inputSerialNo
+    );
+    await axios
+    .post("/api/QA_ORT_WorkingRecord/Search_QA_ORT_WorkingRecord", {
+      PtrList: {
+        ptrFactory: drpFactory,
+        ptrProductType: drpProductType,
+        ptrInput: drpInPut,
+        ptrOutput: drpOutPut,
+        ptrProductName: inputProductName,
+        ptrTestItem: inputTestItem,
+        ptrLotNo: inputLotNo,
+        ptrWeekNo: inputWeekNo,
+        ptrSerialNo: inputSerialNo,
+        ptrDateFrom1: selectedDateFromIn,
+        ptrDateFrom2: selectedDateFromOut,
+        ptrDateTo1: selectedDateToIn,
+        ptrDateTo2: selectedDateToOut
+      },
+    })
+    .then((res) => {
+      let data = res.data.flat().flat();
+      console.log("data ibtExcel_Click", data);
+    });
+  };
+
+  const Btn_Cancel = async () => {
+    console.log("Btn_Cancel");
+    setSelectedFromIn(null);
+    setSelectedFromOut(null);
+    setSelectedToIn(null);
+    setSelectedToOut(null);
+    setDrpFactory("");
+    setDrpProductType("");
+    setDrpInPut("");
+    setDrpOutPut("");
+    setInputProductName(null);
+    setInputTestItem(null);
+    setInputLotNo(null);
+    setInputWeekNo(null);
+    setInputSerialNo(null);
+  };
+  return {
+    opFactory,
+    opProduct_Type,
+    opInput,
+    opOutput,
+    setSelectedFromIn,
+    selectedDateFromIn,
+    setSelectedFromOut,
+    selectedDateFromOut,
+    setSelectedToIn,
+    selectedDateToIn,
+    setSelectedToOut,
+    selectedDateToOut,
+    drpFactory,
+    setDrpFactory,
+    drpProductType,
+    setDrpProductType,
+    drpInPut,
+    setDrpInPut,
+    drpOutPut,
+    setDrpOutPut,
+    setInputProductName,
+    inputProductName,
+    inputTestItem,
+    setInputTestItem,
+    inputLotNo,
+    setInputLotNo,
+    inputWeekNo,
+    setInputWeekNo,
+    inputSerialNo,
+    setInputSerialNo,
+    Btn_Search,
+    Btn_Cancel,
+  };
 }
 
 export { fn_QA_ORT_WorkingRecord };
